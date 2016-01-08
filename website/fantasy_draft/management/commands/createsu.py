e6caed5eba1c72ad86ff6554409878846dba2485
Owen Jow <owenjow@berkeley.edu>
2016-01-08 03:39:23 -0800

    Configured db settings and added a "createsu" command

---
+++ website/fantasy_draft/management/commands/createsu.py
@@ -0,0 +1,7 @@
+from django.core.management.base import BaseCommand
+from django.conf import settings
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        if not settings.AUTH_USER_MODEL.objects.filter(username="admin").exists():
+            settings.AUTH_USER_MODEL.objects.create_superuser("admin", "admin@admin.com", "admin")
