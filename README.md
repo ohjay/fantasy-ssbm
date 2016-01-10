0073768333968f489a0b86706676c743683820a1
Owen Jow <owenjow@berkeley.edu>
2016-01-10 03:54:48 -0800

    Wrote a Genesis 3 player scraper

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 # Fantasy SSBM
 A website that enables the streamlined creation and management of SSBM (Super Smash Bros: Melee) fantasy leagues. Includes facilities for objective player rankings.
 
-View the public rendition here: ~~http://fantasy-ssbm.appspot.com~~ LINK BROKEN; PLANNING ON SWITCHING PRODUCTION SERVERS IN A FEW DAYS.
+View the public rendition here: http://fantasy-ssbm.elasticbeanstalk.com/.
 
 ## Shoutouts
 - [FlipClock.js](http://flipclockjs.com/): used to create the countdown timer on the main page
 - [Django](https://www.djangoproject.com/): used to create everything else
+- [AWS Elastic Beanstalk](https://aws.amazon.com/elasticbeanstalk/): used for deployment
---
+++ website/fantasy_draft/management/commands/addg3players.py
@@ -0,0 +1,58 @@
+from django.core.management.base import BaseCommand
+from ...models import Player, Tournament
+
+import re
+import requests
+import json
+import datetime
+from bs4 import BeautifulSoup
+
+class Command(BaseCommand):
+    help = 'Scrapes the Smash.gg site for tournament entrants'
+    
+    def handle(self, *args, **options):
+        # Tournament creation
+        genesis3 = Tournament.objects.filter(name="Genesis 3")
+        if not genesis3:
+            genesis3 = Tournament.objects.create(name="Genesis 3", date=datetime.date(2016, 1, 15))
+        else:
+            genesis3 = genesis3[0]
+        
+        # Scraping
+        BASE_URL = 'https://smash.gg/tournament/genesis-3/attendees?per_page=50&filter=%7B%22eventIds%22%3A10617%7D&page='
+        PATTERN_A = '"attendee":(\{.*\}\})\}\}\},"plugins"'
+        PATTERN_B = '"attendee":(\{.*\}\})\},"EntrantStore"'
+        
+        for i in range(1, 38):
+            self.stdout.write('Scraping page ' + str(i) + '...')
+            
+            page = requests.get(BASE_URL + str(i))
+            soup = BeautifulSoup(page.content, "lxml")
+            
+            script_contents = soup('script', attrs={
+                'type': None, 
+                'src': None,
+            })[0] # we want the first script without a 'type' attribute
+            
+            match = re.search(PATTERN_A, str(script_contents))
+            try:
+                attendees = json.loads(match.group(1))
+            except AttributeError:
+                match = re.search(PATTERN_B, str(script_contents))
+                attendees = json.loads(match.group(1))
+              
+            for player_wrapper in attendees.values():
+                player = player_wrapper['player']
+                prefix, name, tag = player['prefix'], player['name'], player['gamerTag']
+                if prefix:
+                    tag = prefix + ' | ' + tag
+                
+                # Create the player and save him/her to the database
+                p = Player.objects.filter(tag=tag)
+                if not p:
+                    p = Player.objects.create(name=name, tag=tag)
+                else:
+                    p = p[0]
+                p.tournaments.add(genesis3)
+                p.save()
+                
